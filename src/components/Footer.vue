2a3af3d7c7f821464770997f50792abf77421647
khatri <ajaybavda17@gmail.com>
2024-09-27 11:36:36 +0530

    clean

--- src/components/Footer.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <footer class="bg-blue-600 text-white text-center p-4">
-    <p>
-      Contact us: info@datamanagement.com | © 2024 Data Management Solutions
-    </p>
-  </footer>
-</template>
