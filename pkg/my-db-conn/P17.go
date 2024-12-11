9ad72fd1bed698ffa2d1f68ae4e4ef9f3af0833f
parthvinchhi <parthvinchhi21112000@gmail.com>
2024-12-11 21:05:09 +0530

    Added db-conn to my 3 servers: p12,p15,p17

---
+++ pkg/my-db-conn/P17.go
@@ -0,0 +1,29 @@
+package my_db_conn
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func PostgreSQL17() {
+	dsn := "host=localhost port=5433 dbname=postgres user=postgres password=Pdv@psql sslmode=disable"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("Failed to get database connection: %v", err)
+	}
+
+	if err = sqlDB.Ping(); err != nil {
+		log.Fatalf("Failed to ping database: %v", err)
+	}
+
+	fmt.Println("Connected")
+}
